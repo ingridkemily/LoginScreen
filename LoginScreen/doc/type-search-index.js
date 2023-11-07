2dfb539d8af790d9cc33166caab4871cf442dfc3
Ingrid Kemily <81453161+ingridkemily@users.noreply.github.com>
2023-11-06 21:07:34 -0300

    Delete LoginScreen directory

--- LoginScreen/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Test","l":"LoginScreen"}];updateSearchResults();
--- LoginScreen/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LoginScreen"},{"p":"Test","l":"LoginScreen"},{"p":"Test","l":"LoginScreenTest"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
